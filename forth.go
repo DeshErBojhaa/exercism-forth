3dc3e70db4031944d3862bd2f3453e3fccd60d68
tamjidahmed <mail.to.tamjid@gmail.com>
2024-10-23 23:51:47 +0600

    fix tests
    Signed-off-by: tamjidahmed <mail.to.tamjid@gmail.com>

--- forth.go
+++ forth.go
@@ -7,10 +7,12 @@ import (
 )
 
 var (
-	ErrEmptyStack  = errors.New("empty stack")
-	ErrIllegalOp   = errors.New("illegal operation")
-	ErrUndefinedOp = errors.New("undefined operation")
-	ErrInvalidEq   = errors.New("invalid equation")
+	ErrEmptyStack      = errors.New("empty stack")
+	ErrIllegalOp       = errors.New("illegal operation")
+	ErrUndefinedOp     = errors.New("undefined operation")
+	ErrInvalidEq       = errors.New("invalid equation")
+	ErrDivByZero       = errors.New("divide by zero")
+	ErrOneValueInStack = errors.New("only one value on the stack")
 )
 
 type data struct {
@@ -23,7 +25,7 @@ func (d *data) solveInput(inputs []string) error {
 		return ErrIllegalOp
 	}
 	val, err := d.solveEq(inputs[1:])
-	if errors.Is(err, ErrInvalidEq) {
+	if err != nil {
 		d.variables[va] = strings.Join(inputs[1:], " ")
 		return nil
 	}
@@ -37,12 +39,77 @@ func (d *data) solveInput(inputs []string) error {
 }
 
 func (d *data) solveEq(inputs []string) ([]int, error) {
-	for i, input := range inputs {
+	tmpInp := make([]string, 0)
+	for _, input := range inputs {
 		if d.variables[input] != "" {
-			inputs[i] = d.variables[input]
+			vv := strings.Split(d.variables[input], " ")
+			tmpInp = append(tmpInp, vv...)
+			continue
+		}
+		if _, err := strconv.Atoi(input); err == nil {
+			tmpInp = append(tmpInp, input)
+			continue
+		}
+		return nil, ErrUndefinedOp
+	}
+	inputs = tmpInp
+	stack := make([]int, 0)
+	for _, input := range inputs {
+		if val, err := strconv.Atoi(input); err == nil {
+			stack = append(stack, val)
+			continue
+		}
+		switch input {
+		case "+", "-", "*", "/":
+			if len(stack) < 2 {
+				if len(stack) == 1 {
+					return nil, ErrOneValueInStack
+				}
+				return nil, ErrEmptyStack
+			}
+			switch input {
+			case "+":
+				stack[len(stack)-2] += stack[len(stack)-1]
+			case "-":
+				stack[len(stack)-2] -= stack[len(stack)-1]
+			case "*":
+				stack[len(stack)-2] *= stack[len(stack)-1]
+			case "/":
+				if stack[len(stack)-1] == 0 {
+					return nil, ErrDivByZero
+				}
+				stack[len(stack)-2] /= stack[len(stack)-1]
+			}
+			stack = stack[:len(stack)-1]
+		case "dup":
+			if len(stack) < 1 {
+				return nil, ErrEmptyStack
+			}
+			stack = append(stack, stack[len(stack)-1])
+		case "drop":
+			if len(stack) < 1 {
+				return nil, ErrEmptyStack
+			}
+			stack = stack[:len(stack)-1]
+		case "swap":
+			if len(stack) < 2 {
+				if len(stack) == 1 {
+					return nil, ErrOneValueInStack
+				}
+				return nil, ErrEmptyStack
+			}
+			stack[len(stack)-2], stack[len(stack)-1] = stack[len(stack)-1], stack[len(stack)-2]
+		case "over":
+			if len(stack) < 2 {
+				if len(stack) == 1 {
+					return nil, ErrOneValueInStack
+				}
+				return nil, ErrEmptyStack
+			}
+			stack = append(stack, stack[len(stack)-2])
 		}
 	}
-	return nil, nil
+	return stack, nil
 }
 
 func Forth(input []string) ([]int, error) {
@@ -63,13 +130,13 @@ func Forth(input []string) ([]int, error) {
 	}
 	for _, line := range input {
 		split := strings.Split(line, " ")
-
 		if split[0] == ":" {
 			if err := d.solveInput(split[1 : len(split)-1]); err != nil {
 				return nil, err
 			}
 			continue
 		}
+		return d.solveEq(split)
 	}
 	return nil, nil
 }
